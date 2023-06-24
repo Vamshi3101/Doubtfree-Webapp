5d1d2045f63f96fac1134d14f92cec04fcff1e94
Vamshi <vamshi3101@gmail.com>
2023-06-24 18:41:24 +0530

    Website starting

---
+++ src/Components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Courses from "./Courses";
+import Login from "./Login";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+function App(){
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path = "/" element = {<Home/>}/>
+                <Route path = "/courses" element = {<Courses/>}/>
+                <Route path = "/login" element = {<Login/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    )
+}
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Footer(){
+    return <h1>Footer</h1>
+}
+export default Footer;
